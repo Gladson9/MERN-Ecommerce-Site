56b21a8b1cc85f968e8cf873b7c664b3f5508d13
Gladson Regan Dsouza <38623440+Gladson9@users.noreply.github.com>
2021-11-18 22:49:58 +0530

    Added sorting of orders

--- controllers/user.js
+++ controllers/user.js
@@ -43,6 +43,7 @@ exports.updateUser = (req, res) => {
 exports.userPurchaseList = (req, res) => {
   Order.find({ user: req.profile._id })
     .populate("user", "_id name")
+    .sort({ createdAt: "desc" })
     .exec((err, order) => {
       if (err) {
         return res.status(400).json({
